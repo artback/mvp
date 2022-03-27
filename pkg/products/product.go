b171a8a2f348cd83749a224abc0cbb03967a80a6
freak <artback@protonmail.com>
2022-03-27 21:19:16 +0200

    fix json output camelCase

--- pkg/products/product.go
+++ pkg/products/product.go
@@ -2,7 +2,7 @@ package products
 
 type Product struct {
 	Name     string `json:"name"`
-	SellerID string `json:"seller_id"`
+	SellerID string `json:"sellerId"`
 	Price    int    `json:"price"`
 	Amount   int    `json:"amount"`
 }
